c992dbf19e69401a3d0f578c7184f9addeeb4fc1
DomFinn <dom.finn@gmail.com>
2022-09-12 16:08:21 +0100

    added silly example

---
+++ TestFileStructure.cs
@@ -0,0 +1,18 @@
+﻿namespace TestProjectWithReflection
+{
+	internal class TestFileStructure
+	{
+		public static string File(string name)
+		{
+			// use path.combine to work on different platforms perhaps
+			return "TestFiles\\TestFixtures\\" + name + ".json";
+		}
+
+		public class NoTestFileException : FileNotFoundException
+		{
+			public NoTestFileException() : base("No test file found with the class name supplied")
+			{
+			}
+		}
+	}
+}
